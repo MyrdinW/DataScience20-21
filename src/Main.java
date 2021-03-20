a7341e149cb6f7a38cda2ac26648416a9f1e7674
w-hulshoff <whulshoff@gmail.com>
2021-03-20 17:12:30 +0100

    regex werkt soortvan

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 
 public class Main {
+    String uitgebreideRegex = "/([A-Za-z,. ]*)?([\t]*)(.+?)([ ]*)?(\\([0-9]*\\))([ ]*)?(\\{.+?\\})?(\\(.+?\\))?([ ]*)?(\\[.+?\\])?([ ]*)?(<[0-9]*>)?/g";
 
     public static void main(String[] args) {
         Reader reader = new Reader();
         Parser parser = new Parser();
         String file = "/databases/actorstest.txt";
-        String regex = "/([A-Za-z, ]*)?([\t]*)(.+?)([ ]*)(\\([0-9]*\\))([ ]*)?(\\(.+?\\))?([ ]*)?(\\[.+?\\])?([ ]*)?([ ]*)?(<[0-9]*>)?/g";
+        String regex = "/([A-Za-z,. ]*)?([\t]*)(.+?)([ ]*)(\\([0-9]*\\))/g";
         ArrayList<String> lijst = reader.Read(file);
 
         for(int i = 0; i < lijst.size(); i++) {
--- src/Parser.java
+++ src/Parser.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class Parser {
 
     public static void Parse(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         boolean matchFound = matcher.find();
         if (matchFound) {
