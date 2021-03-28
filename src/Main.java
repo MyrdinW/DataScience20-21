3a6f5f86845e38776492ad904f83005336de26f5
w-hulshoff <whulshoff@gmail.com>
2021-03-28 22:20:06 +0200

    new regex

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Reader reader = new Reader();
         Parser parser = new Parser();
-        String file = "/databases/actorstest.txt";
-        String regex = "([A-Za-z,.'$& ]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
+        String file = "/databases/actors.txt";
+        String regex = "([A-Za-zÀ-ú,.'$&* ]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
         ArrayList<String> lijst = reader.Read(file);
         ArrayList<ArrayList<String>> parsed = parser.ParseActors(lijst, regex);
 
