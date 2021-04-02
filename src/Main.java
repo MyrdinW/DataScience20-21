74e8c988fea5eedb78a73789712feb97ce9c1bd7
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-04-03 00:17:47 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     //String uitgebreideRegex = "/([A-Za-z,. ]*)?([\t]*)(.+?)([ ]*)?(\\([0-9]*\\))([ ]*)?(\\{.+?\\})?(\\(.+?\\))?([ ]*)?(\\[.+?\\])?([ ]*)?(<[0-9]*>)?/g";
 
     public static void main(String[] args) {
-        String file = "/databases/actresses.txt";
-        String regex = "([A-Za-z,.'$&*,-/,0-9,@?,\"]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
+        String file = "/databases/actorstest.txt";
+        String regex = "([A-Za-z,.'$&*,-/,0-9,@?,\" ]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
 
         Reader reader = new Reader();
         Parser parser = new Parser();
@@ -14,8 +14,8 @@ public class Main {
         ArrayList<String> list = reader.Read(file);
         ArrayList<ArrayList<String>> parsed = parser.Parse(list, regex);
      
-        writer.createFile("Actresses");
-        writer.writeToFile(parsed, "Actresses");
+        writer.createFile("Actors");
+        writer.writeToFile(parsed, "Actors");
  
         
     }
