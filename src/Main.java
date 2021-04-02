1d814941488329db2a2a8bff1938aead6ef8cde1
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-04-02 23:36:56 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,17 @@ public class Main {
     //String uitgebreideRegex = "/([A-Za-z,. ]*)?([\t]*)(.+?)([ ]*)?(\\([0-9]*\\))([ ]*)?(\\{.+?\\})?(\\(.+?\\))?([ ]*)?(\\[.+?\\])?([ ]*)?(<[0-9]*>)?/g";
 
     public static void main(String[] args) {
-        String file = "/databases/actors.txt";
-        String file2 = "/databases/directors.txt";
-        String regex = "([A-Za-z,.'$&*,-/,0-9,@?]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
+        String file = "/databases/actorstest.txt";
+        String regex = "([A-Za-z,.'$&*,-/,0-9,@?,\"]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
 
         Reader actorReader = new Reader();
         Parser actorParser = new Parser();
         Writer actorWriter = new Writer();
 
-        Reader directorReader = new Reader();
-        Parser directorParser = new Parser();
-        Writer directorWriter = new Writer();
-
         ArrayList<String> actorList = actorReader.Read(file);
         ArrayList<ArrayList<String>> parsedActors = actorParser.Parse(actorList, regex);
-
-        ArrayList<String> directorList = directorReader.Read(file2);
-        ArrayList<ArrayList<String>> parsedDirectors = directorParser.Parse(directorList, regex);
      
-        directorWriter.createFile("Directors");
         actorWriter.createFile("Actors");
-
-        directorWriter.writeToFile(parsedDirectors, "Directors");
         actorWriter.writeToFile(parsedActors, "Actors");
  
         
