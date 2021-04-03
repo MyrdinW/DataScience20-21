0726836ded7e93154b7a43887267e3bd6d5b95cb
Myrdin <myrdin.willemse@student.nhlstenden.com>
2021-04-03 12:51:54 +0200

    Parser user input (werkt alleen voor actors en actresses

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     //String uitgebreideRegex = "/([A-Za-z,. ]*)?([\t]*)(.+?)([ ]*)?(\\([0-9]*\\))([ ]*)?(\\{.+?\\})?(\\(.+?\\))?([ ]*)?(\\[.+?\\])?([ ]*)?(<[0-9]*>)?/g";
 
     public static void main(String[] args) {
-        String file = "/databases/actresses.txt";
+        // Using Scanner for Getting Input from User
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give me the path to the database you would like to parse: (example: /databases/actresses.txt) ");
+        String file = in.nextLine();
+        
+
+       //String file = "/databases/actresses.txt";
         String regex = "([A-Za-z,.'$&*,-/,0-9,@?,\" ]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
 
         Reader reader = new Reader();
         Parser parser = new Parser();
         Writer writer = new Writer();
-
+        //removeExtension remover = new removeExtension();
+        
         ArrayList<String> list = reader.Read(file);
         ArrayList<ArrayList<String>> parsed = parser.Parse(list, regex);
-     
-        writer.createFile("Actresses");
-        writer.writeToFile(parsed, "Actresses");
+        String newFile = removeExtension.RemoveExtension(file);
+       
+        writer.createFile(newFile);
+        writer.writeToFile(parsed, newFile);
  
         
     }
---
+++ src/removeExtension.java
@@ -0,0 +1,23 @@
+public class removeExtension{
+    
+    public static String RemoveExtension(String s) {
+
+   
+    String filename;
+
+    // Remove the path upto the filename.
+    int pos = s.lastIndexOf("/") + 1;
+    if (pos == -1) {
+        filename = s;
+    } else {
+        filename = s.substring(s.lastIndexOf("/")+1);
+    }
+
+    // Remove the extension.
+    int extensionIndex = filename.lastIndexOf(".");
+    if (extensionIndex == -1)
+        return filename;
+
+    return filename.substring(0, extensionIndex);
+}
+}
