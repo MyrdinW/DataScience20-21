e85e598478aa784fb94897c1b9a9e63cc1ef6176
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-04-03 19:06:37 +0200

    Update dataScience.R

--- R Scripts/dataScience.R
+++ R Scripts/dataScience.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+
 rating <- read.csv("~/GitHub/DataScience20-21/SQL Scripts/Data CSV/rating.csv")
 actor <- read.csv("~/GitHub/DataScience20-21/SQL Scripts/Data CSV/actor.csv")
 actress <- read.csv("~/GitHub/DataScience20-21/SQL Scripts/Data CSV/actress.csv")
@@ -9,4 +10,10 @@ movie <- read.csv("~/GitHub/DataScience20-21/SQL Scripts/Data CSV/movie.csv")
 rating2 <- select(rating, Votes, Rating)
 rating2
 
-plot(rating2)
+science_fiction_movies <- filter(genre, Genre =="Science Fiction" )
+science_fiction_movies <- select(science_fiction_movies, Title, Genre)
+
+science_fiction_movies
+
+sumOfSFMovies <- count(science_fiction_movies)
+sumOfSFMovies
