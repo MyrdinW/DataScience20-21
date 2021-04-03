7ca90f45fdcec106a38163e5b9095d29b96f9099
Myrdin <myrdin.willemse@student.nhlstenden.com>
2021-04-03 16:28:35 +0200

    parserCountries toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         String regexActor = "([A-Za-z,.'$&*,-/,0-9,@?,\" ]*)?([\t]*)(.+?)([ ]*)\\(([0-9,?]{4})(.+?\\n{2})?";
         String regexRating = "(.{27})([0-9]\\.[0-9])([ ]*)([A-Za-zÀ-ú,.'$&*,-/,0-9,#!\"@?:, ]*)(\\()([0-9,?]{4})(\\))([ ]*)([A-Za-zÀ-ú,.'$&*,-/,0-9,#!\"@?{}():, ]*)";
         String regexGenres = "(.+?) \\(([0-9]{4})(/[I]*)?\\) ?(\\{.+?\\}?\\})?(\\(.+?\\))?(\\t*)([A-z\\-]*)";
+        String regexCountries = "([A-Za-zÀ-ú,.'$&*,-/,0-9,#!\"@?:, ]*)(\\()([0-9,?]{4})(\\))([A-Za-zÀ-ú,.'$&*,-/,0-9,#!\"@?{}():, ]*)([\t]*)([A-Za-z,.\s -].+)";
 
         Reader reader = new Reader();
         Writer writer = new Writer();
@@ -37,6 +38,12 @@ public class Main {
             ParserGenres parserGenres = new ParserGenres();
             ArrayList<String> list = reader.Read(file);
             ArrayList<ArrayList<String>> parsed = parserGenres.Parse(list, regexGenres);
+            writer.writeToFile(parsed, newFile);
+        }  else if(newFile.equalsIgnoreCase("countries")){
+            writer.createFile(newFile);
+            ParserCountries parserCountries = new ParserCountries();
+            ArrayList<String> list = reader.Read(file);
+            ArrayList<ArrayList<String>> parsed = parserCountries.Parse(list, regexCountries);
             writer.writeToFile(parsed, newFile);
         } else { 
             System.out.println("Verkeerd bestand gekozen.");
---
+++ src/ParserCountries.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class ParserCountries {
+
+    public ArrayList<ArrayList<String>> Parse(ArrayList<String> input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+
+
+        ArrayList<ArrayList<String>> collectie = new ArrayList<ArrayList<String> >();
+
+        collectie.add(new ArrayList<String>());
+
+        collectie.get(0).add("Movie Title");
+        collectie.get(0).add("Year");
+        collectie.get(0).add("Land");
+
+        for (int i = 0; i < input.size(); i++) {
+            Matcher matcher = pattern.matcher(input.get(i));
+
+            collectie.add(new ArrayList<String>());
+
+            if(matcher.find()) {
+
+                if (matcher.group(1) != null) {
+                    String movie = matcher.group(1).toString();
+                    //lijst.add(movie.replaceAll("[,]", ""));
+
+                    if(movie == ""){
+                        collectie.get(i+1).add(collectie.get(i).get(0).toString().replaceAll("[']", ""));
+                        //System.out.println(collectie.get(i+1).get(0));
+
+                    } else {
+                        collectie.get(i+1).add(movie.replaceAll("[,]", ""));
+                        //System.out.println(collectie.get(i+1).get(0));
+                    }
+                }
+
+                if (matcher.group(3) != null) {
+                    //lijst.add(matcher.group(3));
+
+                    collectie.get(i+1).add(matcher.group(3).toString());
+                }
+
+                if (matcher.group(7) != null) {
+                    //lijst.add(matcher.group(5));
+                    collectie.get(i+1).add(matcher.group(7));
+                }
+            }
+
+        }
+
+        return collectie;
+    }
+}
